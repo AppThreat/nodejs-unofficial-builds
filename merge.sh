9da568ee28ce32263d3353aeb7f4f033b97c5be8
Roland Asmann <roland.asmann@gmail.com>
2025-12-11 23:26:46 +0000

    Fixed merge script and index files
    Signed-off-by: Roland Asmann <roland.asmann@gmail.com>

--- merge.sh
+++ merge.sh
@@ -5,8 +5,6 @@ cd dists/v$1
 sha256sum * > SHASUMS256.txt
 cd ..
 npx nodejs-dist-indexer --dist . --indexjson index.json --indextab index.tab
-sed -i '' 's/linux-x64-musl/linux-arm64-musl","linux-x64-musl/g' index.json
-sed -i '' 's/linux-x64-musl/linux-arm64-musl,linux-x64-musl/g' index.tab
 git add index.* v$1/*
 git commit -sm "Added arm64-musl v$1 binaries, checksums and indices"
 git push
